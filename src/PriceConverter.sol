2a01d7a83f89286e2c58965605acf8eb4e56d091
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-11-11 20:09:26 +0200

    Fix the reference to chainlink contracts

--- src/PriceConverter.sol
+++ src/PriceConverter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
     function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
